8f9381d295e0ae13c1644d9cfb5461db7d333be3
LiquidAssContainer <LiquidAssContainer@users.noreply.github.com>
2020-12-12 16:40:38 +0500

    Added webpack-merge & config separation

---
+++ webpack.prod.js
@@ -0,0 +1,10 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = merge(common, {
+	mode: "production",
+	optimization: {
+		minimizer: [new OptimizeCSSAssetsPlugin({})],
+	},
+});
